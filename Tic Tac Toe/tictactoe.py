1bf84bda28fcc823f2117d98bda926dbc09a8fa7
MickSher <mickysheridan@icloud.com>
2021-12-20 16:11:37 -0600

    Instantiated board

--- Tic Tac Toe/tictactoe.py
+++ Tic Tac Toe/tictactoe.py
@@ -14,7 +14,9 @@ WHITE = (255,255,255)
 
 
 
-
+#Initialization
+mousex = 0
+mousey = 0
 #Instantiate Window
 screenHeight = 800
 screenWidth = 800
@@ -27,8 +29,12 @@ myfont = pg.font.SysFont("monospace", 12)
 hud = myfont.render("Tic Tac Toe", 1, BLUE)
 #blit the font on the screen at location 1, 1
 
+board = [[0,0,0],[0,0,0],[0,0,0]]
 
-
+#0's or 1's will determine x's or o's
+#for loop for finding 0's and 1's within board
+#if statement for each piece of information in board to print either x or o
+#update screen
 
 playing = True
 
@@ -38,13 +44,26 @@ while playing:
             sys.exit()
         if event.type == pg.MOUSEBUTTONDOWN:
             mousex, mousey = pg.mouse.get_pos()
-        
-        
+            print(mousex, mousey)
+    
     screen.fill(WHITE)
     screen.blit(hud, ((screenWidth/2) - 20, 10))
+
+    pg.draw.line(screen, RED, (0, screenHeight/3),(screenWidth, screenHeight/3), 5)
+    pg.draw.line(screen, RED, (0, screenHeight*(2/3)),(screenWidth, screenHeight*(2/3)), 5)
+    pg.draw.line(screen, RED, (screenWidth/3, 0),(screenWidth/3, screenHeight), 5)
+    pg.draw.line(screen, RED, (screenWidth*(2/3), 0),(screenWidth*(2/3), screenHeight), 5)
+
+    #if player 1 (not specified) clicks within certain area, two lines are drawn to form an X.
+
+
+    if (mousex < screenWidth/3) and (mousey < screenHeight/3):
+        
+        #pg.draw.line(screen, BLUE, (screenWidth*(3/99), screenHeight*(3/99)), (screenWidth*(30/99), screenHeight*(30/99)), 5)
+    #if player 2 (not specified) clicks within certain area, circle is drawn.
+    #Sizes of lines and circles shouldn't intersect with board lines.
+
     pg.display.update()
-
-
 
 
 #While loop for gameplay
@@ -57,9 +76,10 @@ while playing:
 #evaluate "loss"
 #evaluate "tie"
 
-"""---Order of Ideas---
+"""---Ideas---
 Player vs Player
 Player vs AI
 AI vs AI
 Difficulty Levels (easy, normal, hard, impossible)
-Player Profiles (logging wins, losses, ties, win%, win streak, total games played)"""
+Player Profiles (logging wins, losses, ties, win%, win streak, total games played)
+Customization between X or O"""
